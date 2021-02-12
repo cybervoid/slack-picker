62110c9b5b9e33f19b228021574279fca797f600
Rafael Gil <rafaelgil83@gmail.com>
2021-02-11 22:18:30 -0500

    - working space names

--- index.js
+++ index.js
@@ -14,9 +14,27 @@ const server = http.createServer((req, res) => {
 
     req.on('end', () => {
         let parsedData = querystring.decode(rawData)
-        res.end(`Hello, ${parsedData.user_name}`);
+        const command = parsedData.command
+        const commandData = parsedData.text
+        const response = parseCommandData(commandData)
+        res.end(`${response}`);
     });
 
 
 })
 server.listen(port, hostname);
+
+function parseCommandData(data) {
+    //check if the list is comma separated
+    let res = `Can't parse the provided list. Please provide a valid list 😕`
+
+    if (data.indexOf(",") === -1) {
+        //comma list not detected, try space separated list
+        if (data.indexOf(" ") !== -1) {
+            const list = data.split(" ");
+            const random = Math.floor(Math.random() * list.length)
+            res = list[random]
+        }
+    }
+    return res
+}
