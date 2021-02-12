f3c7d861895810b1e249b6885584c2ebd43c51f9
Rafael Gil <rafaelgil83@gmail.com>
2021-02-11 21:02:39 -0500

    - created local server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+
+    res.end(`<html><body><h1>Hello, World!</h1></body></html>`);
+})
+server.listen(port, hostname);
